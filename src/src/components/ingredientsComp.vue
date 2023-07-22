22f5e6f92babbed46da438ac6a9dc8535dc8834d
roihas1 <103654352+roihas1@users.noreply.github.com>
2023-07-22 23:57:39 +0300

    fixing my recipes,family recipes

---
+++ src/src/components/ingredientsComp.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="container">
+        <h3>Ingredients</h3>
+        <div v-if = "isObject" >
+            <ul class="ingredient-list">
+                <li class="bullet" v-for="(ingredient,index) in ingredients" :key="index"> <strong>{{ index.charAt(0).toUpperCase()+index.slice(1) }}</strong>: {{ ingredient }}</li>
+            </ul>
+        </div>
+        <div v-else class="ingredient-list">
+            {{ ingredients }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+        isObject:true,
+        }
+    },
+props: {
+    ingredients: {
+        type: Object,
+        required: true
+    }
+},
+mounted(){
+    console.log("typeof: ",typeof this.ingredients);
+    if (typeof this.ingredients === "string"){
+        this.isObject = false;
+    }
+}
+
+}
+</script>
+
+<style scoped>
+.container{
+    justify-content: left;
+    align-content: left;
+    background: #edf5e1;
+    border-radius: 2rem;
+    width:100%;
+    font-family: 'libre franklin', sans-serif;
+    padding: 1rem;
+}
+.bullet::marker{
+    color:#42b983;
+}
+.ingredient-list {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr); 
+  gap: 10px; 
+  padding: 0; 
+  margin:1rem;
+
+}
+
+</style>
---
+++ src/src/pages/AboutPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <h1 class="title">About Page</h1>
+    <br/>
+
+    <h1>About Us</h1>
+    
+  <p>Hello! We are Tal and Roi, two students studying Software and Information Systems Engineering. Together, we embarked on a project to create a website focused on recipes, aiming to provide a valuable resource for cooking enthusiasts and food lovers.</p>
+<br/>
+  <h1>About Recipes Website Project</h1>
+  <p>Our recipes website project combines our technical skills with our love for food and cooking. We offer an extensive collection of recipes from various cuisines, dietary preferences, and cooking techniques. Our platform provides an intuitive user interface and interactive cooking guides.</p>
+<br/>
+  <h2>Our Last Projects</h2>
+    <p><a href="https://web-development-environments-2023.github.io/316120054/">Morgan Freeman website</a></p>
+    <p><a href="https://web-development-environments-2023.github.io/208980920/">Mordechai Chibotero website</a></p>
+    <p><a href="https://web-development-environments-2023.github.io/assignment2-316120054_208980920/">StarShip Game</a></p>
+  <h2>Videos</h2>
+  <p><a href="https://www.youtube.com/watch?v=COEof8qZK34">Niv Gilboa Recommended Review</a></p>
+  <p><a href="https://www.youtube.com/watch?v=5zbEvK5hIe0">Niv Gilboa Unrecommended Review</a></p>
+
+  <p>Thank you for joining us on this exciting journey of flavors, creativity, and technology!</p>
+
+  <p class="names">- Tal Abraham and Roi Hass-</p>
+  </div>
+ 
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.title{
+  text-align: center;}
+.names{
+  text-align: center;
+  font-size: 1.5rem;
+}
+</style>
