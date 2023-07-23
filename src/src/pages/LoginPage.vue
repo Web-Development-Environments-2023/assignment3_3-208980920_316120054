cf4a5919c8113e3add1941135870d5c53369e405
roihas1 <103654352+roihas1@users.noreply.github.com>
2023-07-23 17:17:13 +0300

    Last major update with cosmetic changes

---
+++ src/src/pages/LoginPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 class="title">Login</h1>
+
+    <LoginCard ></LoginCard>
+  </div>
+</template>
+
+<script>
+import LoginCard from "../components/LoginCard";
+export default {
+  name:"Login",
+  components: {
+    LoginCard
+  },
+
+};
+</script>
+
